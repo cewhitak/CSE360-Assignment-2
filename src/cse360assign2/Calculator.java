675e7b4ef3863d001d3273502f8fb8a20c75f6d2
Courtney Whitaker <cewhitak@asu.edu>
2019-02-21 16:46:12 -0700

    Assignment 2 part 1

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,43 @@
+package cse360assign2;
+
+/**
+ * Name: Courtney Whitaker
+ * ClassID: 589
+ * Assignment #2
+ * Description: This code will add, subtract, multiple, and divide a number by a submitted
+ * value. The program will also print a string of all the calculations that were completed.  
+ */ 
+
+
+public class Calculator {
+	private int total;
+	
+	public Calculator () {
+		total = 0;  
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	 
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+
+}
