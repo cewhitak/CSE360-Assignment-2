ada627920e06473fb237aa0cd2f10b70cea974fa
Courtney Whitaker <cewhitak@asu.edu>
2019-02-22 10:42:39 -0700

    Assignment 2 part 2. This is the correct second version of the file for grading

--- src/cse360assign2/Calculator.java
+++ src/cse360assign2/Calculator.java
@@ -8,30 +8,78 @@ package cse360assign2;
  * value. The program will also print a string of all the calculations that were completed.  
  */ 
 
+/**
+ * The Calculator class creates a calculator object that can add, subtract, multiply, 
+ * and divide 
+ * @author Courtney2
+ *
+ */
 
 public class Calculator {
 	private int total;
 	
+	/**
+	 * Calculator is the constructor that sets the calculator to zero
+	 * @author Courtney2
+	 *
+	 */
+	
 	public Calculator () {
 		total = 0;  
+		
 	}
+	
+	/**
+	 * getTotal returns the current value of total, the value of the calculator after
+	 * all calculations have been completed
+	 * @author Courtney2
+	 *
+	 */
 	
 	public int getTotal () {
 		return total;
 	}
 	 
+	/**
+	 * add method adds a value to the current total value in the calculator
+	 * @author Courtney2
+	 *
+	 */
+	
 	public void add (int value) {
 		total= total + value;
+		
 	}
+	
+	/**
+	 * subtract method subtracts a value from the current total value in the calculator
+	 * @author Courtney2
+	 *
+	 */
 	
 	public void subtract (int value) {
 		total= total - value;
+		
 	}
+	
+	/**
+	 * multiply method multiplies a value by the current total value in the calculator
+	 * @author Courtney2
+	 *
+	 */
 	
 	public void multiply (int value) {
 		total= total * value;
-		System.out.println(total);
+		
+		
 	}
+	
+	/**
+	 * divide method divides a value from the current total value in the calculator
+	 * and if the value is 0 the total is set to 0
+	 * @author Courtney2
+	 *
+	 */
 	
 	public void divide (int value) {
 		if(value == 0)
@@ -43,12 +91,19 @@ public class Calculator {
 		{
 			total= total / value;
 		}
-		System.out.println(total);
+		
 	}
+	
+	/**
+	 * getHistory method returns an empty string
+	 * @author Courtney2
+	 *
+	 */
 	
 	public String getHistory () {
 		return "";
 	}
+	
 
 }
 
